47d839f9d3b1a08d72d963ac06e51f530931b937
kickbuttowski80 <pazpaz25@gmail.com>
2020-01-28 16:29:35 -0800

    checkout info has its own compoenent

---
+++ src/components/CheckoutButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+          <h1>Cart Total So far: </h1>
+          <v-btn color= "success"
+                >{{total | currency}}
+          </v-btn>
+          <v-btn  color='warning'                
+          v-on:click="checkout">Checkout
+          </v-btn> 
+          <span v-if="checkoutStatus">{{checkoutStatus}}</span> 
+    
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapState} from 'vuex'
+export default {
+    
+  computed:{
+    
+        // cartTotal(){
+        //     return this.$store.getters.cartTotal
+        // },
+        //   checkout(){ 
+        //     return this.$store.dispatch('checkout')
+        // }
+
+        ...mapGetters({
+           total: 'cartTotal'
+        }),
+        ...mapActions({
+          checkout: 'checkout'
+        }),
+        ...mapState({
+          checkoutStatus: state => state.checkoutStatus
+         
+        })
+  }
+}
+</script>
